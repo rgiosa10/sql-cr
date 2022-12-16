7938e5010b3923886661df5a4a302447e4721d7d
Ruben Giosa <rubenalexgiosa@gmail.com>
2022-12-16 08:47:02 -0800

    Add code to complete second query of Selecting values section

--- main.sql
+++ main.sql
@@ -4,3 +4,9 @@
 SELECT name, part_num
 FROM parts
 WHERE name LIKE '%Hair%' ORDER BY part_num DESC;
+
+-- From the sets table, select all the sets that included the word 'Showdown' in the name between 1990 and 2015
+
+SELECT *
+FROM sets
+WHERE name like '%Showdown%' AND (year BETWEEN 1990 and 2015);
